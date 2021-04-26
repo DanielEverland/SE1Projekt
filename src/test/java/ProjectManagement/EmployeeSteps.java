9b53aceeac9afe4d2730c94186469c9f54c1fd46
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2021-04-26 14:43:09 +0200

    assertion fix

--- src/test/java/ProjectManagement/EmployeeSteps.java
+++ src/test/java/ProjectManagement/EmployeeSteps.java
@@ -29,6 +29,9 @@ public class EmployeeSteps {
 		assertTrue(projectTasks.stream()
 				.anyMatch(m -> m.getTitle().contentEquals(title) && m.getDescription().contentEquals(description)));
 		// ikke godt ^. Checker kun om titel og description matcher
+		assertThat(holder.project.findTask(title, description, startDate, endDate).getStartDate(), is(equalTo(startDate)));
+		assertThat(holder.project.findTask(title, description, startDate, endDate).getEndDate(), is(equalTo(endDate)));
+		// så det bliver fikset sådan her selvom det er grimt
 	}
 
 	@When("the employee inputs {double} hours worked on the task with title {string}, description {string}, start date {int} and end date {int}")
@@ -37,8 +40,7 @@ public class EmployeeSteps {
 		holder.project.findTask(title, description, startDate, endDate).logWorkHours(holder.employee, hoursWorked);
 
 	}
-
-	@Then("{double} hours is registered as worked on the task with title {string}, description {string}, start date {int} and end date {int}  by the employee")
+	@Then("{double} hours is registered as worked on the task with title {string}, description {string}, start date {int} and end date {int} by the employee")
 	public void hours_is_registered_as_worked_on_the_task_with_title_description_start_date_and_end_date_by_the_employee(
 			Double hoursWorked, String title, String description, Integer startDate, Integer endDate) {
 		Map<Employee, Duration> minutesWorked = holder.project.findTask(title, description, startDate, endDate)
@@ -46,5 +48,21 @@ public class EmployeeSteps {
 		Duration duration = minutesWorked.get(holder.employee);
 		assertThat(duration.GetHoursPassed(), is(equalTo(hoursWorked)));
 	}
+	@Given("the task with title {string}, description {string}, start date {int} and end date {int} has expected time of {double} hours")
+	public void the_task_with_title_description_start_date_and_end_date_has_expected_time_of_hours(String title, String description, Integer startDate, Integer endDate, Double expectedTime) {
+		holder.project.findTask(title, description, startDate, endDate).setExpectedTime(expectedTime);
+	}
+	
+	@Given("worked time of {double} hours is larger than expected time of {double} hours")
+	public void worked_time_of_hours_is_larger_than_expected_time_of_hours(Double workedHours, Double expectedHours) {
+		assertTrue(workedHours > expectedHours);
+	}
 
+	@Then("the employee get the error message {string}")
+	public void the_employee_get_the_error_message(String errorMessage) throws AuthException {
+		
+		assertThat(1, is(equalTo(2)));
+		// how 2 mak
+	}
+	
 }
