50bc1c5e19203b3032a0b33f4b74da5b3b8e45f3
Daniel Everland <danieleverland@gmail.com>
2021-05-01 14:18:57 +0200

    Added more scenarios to duration feature

--- src/main/java/ProjectManagement/Duration.java
+++ src/main/java/ProjectManagement/Duration.java
@@ -5,7 +5,7 @@ public class Duration {
 	private static final double EPSILON = 0.00001; 
 	private long MinutesPassed;
 	
-	public void AddTime(int hours, int minutes) {
+	public void AddTime(Double hours, int minutes) {
 		AddHours(hours);
 		AddMinutes(minutes);
 	}
@@ -14,12 +14,6 @@ public class Duration {
 		assert minutes > 0;
 		
 		MinutesPassed += minutes;
-	}
-	
-	public void AddHours(int hours) {
-		assert hours > 0;
-		
-		MinutesPassed += hours * 60;
 	}
 	
 	public void AddHours(double hours) {
@@ -37,18 +31,6 @@ public class Duration {
 	
 	public double GetHoursPassed() {
 		return MinutesPassed / 60.0;
-	}
-	
-	public boolean HaveHoursPassed(int hours) {
-		assert hours > 0;
-		
-		return MinutesPassed >= hours * 60;
-	}
-	
-	public boolean HaveMinutesPassed(long minutes) {
-		assert minutes > 0;
-		
-		return MinutesPassed >= minutes;
 	}
 
 	public static double getEpsilon() {
