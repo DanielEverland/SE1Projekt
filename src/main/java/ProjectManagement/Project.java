09d3848b6214986f19423a3bab2815466f8a7ab6
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2021-05-03 20:06:09 +0200

    fixed merge conflict

--- src/main/java/ProjectManagement/Project.java
+++ src/main/java/ProjectManagement/Project.java
@@ -6,66 +6,65 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Project {
-    // The number of digits in the stringified id of projects
-    private static final int serialDigits = 4;
+	// The number of digits in the stringified id of projects
+	private static final int serialDigits = 4;
 
-    private int year;
-    private int id;
-    private String title;
-    private ArrayList<Task> tasks;
-    private Employee projectLead;
+	private int year;
+	private int id;
+	private String title;
+	private ArrayList<Task> tasks;
+	private Employee projectLead;
 	private boolean completed;
 
-    public Project(int id, String title) {
-        Assert.assertFalse("Id must be a non-negative integer", id < 0);
-        Assert.assertFalse("Title cannot be empty or contain '|'", title.length() == 0 || title.contains("|"));
+	public Project(int id, String title) {
+		Assert.assertFalse("Id must be a non-negative integer", id < 0);
+		Assert.assertFalse("Title cannot be empty or contain '|'", title.length() == 0 || title.contains("|"));
 
-        year = Calendar.getInstance().get(Calendar.YEAR);
-        this.id = id;
-        this.title = title;
-        tasks = new ArrayList<Task>();
-    }
+		year = Calendar.getInstance().get(Calendar.YEAR);
+		this.id = id;
+		this.title = title;
+		tasks = new ArrayList<Task>();
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getFullTitle() {
-        return year + "|" + idToString() + "|" + title;
-    }
+	public String getFullTitle() {
+		return year + "|" + idToString() + "|" + title;
+	}
 
-    private String idToString() {
-        return String.format("%0" + serialDigits + "d", id);
-    }
+	private String idToString() {
+		return String.format("%0" + serialDigits + "d", id);
+	}
 
-    public void assignProjectLeader(Employee newProjectLeader) {
-    	projectLead = newProjectLeader;
-    }
+	public void assignProjectLeader(Employee newProjectLeader) {
+		projectLead = newProjectLeader;
+	}
 
-    public void createTask(TaskConstructorInfo info) {
-    	if(!info.isValid())
-    	{
-    		ErrorMessageHandler.addErrorMessage("Constructor info contains invalid information");
-    		return;
-    	}
-    	
-    	tasks.add(new Task(info));
-    }
+	public void createTask(TaskConstructorInfo info) {
+		if (!info.isValid()) {
+			ErrorMessageHandler.addErrorMessage("Constructor info contains invalid information");
+			return;
+		}
 
-    public boolean containsTask(String title, String description, Date startDate, Date endDate) {
-    	return findTask(title, description, startDate, endDate) != null;
-    }
-    
-    public void assignTaskToEmployee(Employee employee, Task task) {    	
-    	if (employee.isAvailable(task)) {
-    		employee.assignToTask(task);
-    	} else {
-    		ErrorMessageHandler.addErrorMessage("Employee is unavailable");
-    	}
+		tasks.add(new Task(info));
+	}
+
+	public boolean containsTask(String title, String description, Date startDate, Date endDate) {
+		return findTask(title, description, startDate, endDate) != null;
+	}
+
+	public void assignTaskToEmployee(Employee employee, Task task) {
+		if (employee.isAvailable(task)) {
+			employee.assignToTask(task);
+		} else {
+			ErrorMessageHandler.addErrorMessage("Employee is unavailable");
+		}
 	}
 
 	public boolean isProjectLeader(Employee employee) {
@@ -78,10 +77,8 @@ public class Project {
 
 	public Task findTask(String title, String description, Date startDate, Date endDate) {
 		for (Task task : tasks) {
-			if(task.getTitle().equals(title) &&
-					task.getDescription().equals(description) &&
-					task.getStartDate().equals(startDate) &&
-					task.getEndDate().equals(endDate)) {
+			if (task.getTitle().equals(title) && task.getDescription().equals(description)
+					&& task.getStartDate().equals(startDate) && task.getEndDate().equals(endDate)) {
 				return task;
 			}
 		}
@@ -90,42 +87,43 @@ public class Project {
 
 	public void editTaskTitle(Task task, String newTitle, Employee employee) {
 		if (isProjectLeader(employee)) {
-			task.editTitle(newTitle);			
+			task.editTitle(newTitle);
 		}
-		
+
 	}
 
 	public void editTaskDescription(Task task, String newDescription, Employee employee) {
-		if(isProjectLeader(employee)) {
+		if (isProjectLeader(employee)) {
 			task.editDescription(newDescription);
 		}
 	}
 
 	public void editTaskStartDate(Task task, Date newStartDate, Employee employee) {
-		if(isProjectLeader(employee)) {
+		if (isProjectLeader(employee)) {
 			task.setStartDate(newStartDate);
-		}		
+		}
 	}
 
 	public void editTaskEndDate(Task task, Date newEndDate, Employee employee) {
-		if(isProjectLeader(employee)) {
+		if (isProjectLeader(employee)) {
 			task.setEndDate(newEndDate);
-		}			
+		}
 	}
 
 	public void editExpectedTimeForTask(Task task, Double newExpectedTime, Employee employee) {
-		if(isProjectLeader(employee)) {
+		if (isProjectLeader(employee)) {
 			task.setExpectedTime(newExpectedTime);
 		}
-		
+	}
 
 	public void markAsCompleted() {
 		if (!completed) {
 			completed = true;
 		} else {
-    		ErrorMessageHandler.addErrorMessage("Project already marked as complete");
+			ErrorMessageHandler.addErrorMessage("Project already marked as complete");
 		}
 	}
+
 	public boolean isCompleted() {
 		return completed;
 	}
