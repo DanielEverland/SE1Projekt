a23e03680594ca62704ee7eac80ec1a05615d67d
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2021-05-03 21:48:57 +0200

    updated edit task feature and steps

--- features/edit_task.feature
+++ features/edit_task.feature
@@ -7,45 +7,45 @@ Scenario: Project leader edits the title of a task
 	And the employee is signed in 
 	And the employee is a project leader 
 	And A task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" exists
-	When the employee edits title to "New title" for task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000"
-	Then A task with title "New title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" exists
-	And the task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" does not exist
+	When the employee edits title to "New title" for task
+	Then A task with title "New title" exists
+	And the task with title "Test Title" does not exist
 	
 Scenario: Project leader edits the description of a task
 	Given an employee with id "test" exists in the application 
 	And the employee is signed in 
 	And the employee is a project leader 
 	And A task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" exists
-	When the employee edits description to "New Description" for task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000"
-	Then A task exists with title "Test Title", description "New Description", start date "31/12/1998" and end date "31/12/2000"
-	And the task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" does not exist
+	When the employee edits description to "New Description" for task
+	Then A task with description "New Description" exists
+	And the task with description "Test Description" does not exist
 	
 Scenario: Project leader edits the start date of a task
 	Given an employee with id "test" exists in the application 
 	And the employee is signed in 
 	And the employee is a project leader 
 	And A task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" exists
-	When the employee edits start date to "01/12/2000" for task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000"
-	Then A task exists with title "Test Title", description "Test Description", start date "01/12/2000" and end date "31/12/2000"
-	And the task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" does not exist
+	When the employee edits start date to "01/12/2000" for task
+	Then A task with start date "01/12/2000" exists
+	And the task with start date "31/12/1998" does not exist
 
 Scenario: Project leader edits the end date of a task
 	Given an employee with id "test" exists in the application 
 	And the employee is signed in 
 	And the employee is a project leader 
 	And A task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" exists
-	When the employee edits end date to "01/12/2000" for task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000"
-	Then A task exists with title "Test Title", description "Test Description", start date "31/12/1998" and end date "01/12/2000"
-	And the task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" does not exist
+	When the employee edits end date to "01/12/2000" for task
+	Then A task with end date "01/12/2000" exists
+	And the task with end date "31/12/2000" does not exist
 	
 Scenario: Project leader edits the expected time of a task
 	Given an employee with id "test" exists in the application 
 	And the employee is signed in 
 	And the employee is a project leader 
 	And A task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" exists
-	And the task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" has expected time of 3.0 hours
-	When the employee edits expected time to 5.0 hours for task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000"
-	Then the task with title "Test Title", description "Test Description", start date "31/12/1998" and end date "31/12/2000" has 5.0 hours of expected time
+	And the task has 3.0 hours of expected time
+	When the employee edits expected time to 5.0 hours for task
+	Then the task now has 5.0 hours of expected time 
 
 
 
--- src/test/java/ProjectManagement/EditTaskSteps.java
+++ src/test/java/ProjectManagement/EditTaskSteps.java
@@ -2,6 +2,8 @@ package ProjectManagement;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,7 +14,8 @@ import io.cucumber.java.en.*;
 public class EditTaskSteps {
 
 	private MainHolder holder;
-
+	private Task task;
+	
 	public EditTaskSteps(MainHolder holder) {
 		this.holder = holder;
 	}
@@ -23,72 +26,98 @@ public class EditTaskSteps {
 		Date startDate = Date.FromString(startDateString);
 		Date endDate = Date.FromString(endDateString);
 		holder.project.createTask(new TaskConstructorInfo(title, description, startDate, endDate));
+		task = holder.project.findTask(title, description, startDate, endDate);
+		
+	}
+	
+	@When("the employee edits title to {string} for task")
+	public void the_employee_edits_title_to_for_task(String title) {
+		holder.project.editTaskTitle(task, title, holder.employee);
 	}
 
-	@When("the employee edits title to {string} for task with title {string}, description {string}, start date {string} and end date {string}")
-	public void the_employee_edits_title_to_for_task_with_title_description_start_date_and_end_date(String newTitle,
-			String title, String description, String startDateString, String endDateString) {
-		Date startDate = Date.FromString(startDateString);
-		Date endDate = Date.FromString(endDateString);
-		Task task = holder.project.findTask(title, description, startDate, endDate);
-		holder.project.editTaskTitle(task, newTitle, holder.employee);
-	}
-
-	@Then("the task with title {string}, description {string}, start date {string} and end date {string} does not exist")
-	public void the_task_with_title_description_start_date_and_end_date_does_not_exist(String title, String description,
-			String startDateString, String endDateString) {
-		Date startDate = Date.FromString(startDateString);
-		Date endDate = Date.FromString(endDateString);
-		Task task = holder.project.findTask(title, description, startDate, endDate);
+	@Then("A task with title {string} exists")
+	public void a_task_with_title_exists(String title) {
 		ArrayList<Task> tasks = holder.project.getTasks();
-		assertThat(tasks, not(hasItem(task)));
+		assertThat(tasks, contains(hasProperty("title", is(title))));
 	}
 
-	@When("the employee edits description to {string} for task with title {string}, description {string}, start date {string} and end date {string}")
-	public void the_employee_edits_description_to_for_task_with_title_description_start_date_and_end_date(
-			String newDescription, String title, String description, String startDateString, String endDateString) {
-		Date startDate = Date.FromString(startDateString);
-		Date endDate = Date.FromString(endDateString);
-		Task task = holder.project.findTask(title, description, startDate, endDate);
+	@Then("the task with title {string} does not exist")
+	public void the_task_with_title_does_not_exist(String title) {
+		ArrayList<Task> tasks = holder.project.getTasks();
+		assertThat(tasks, contains(hasProperty("title", not(is(title)))));
+	}
+
+	@When("the employee edits description to {string} for task")
+	public void the_employee_edits_description_to_for_task(String newDescription) {
 		holder.project.editTaskDescription(task, newDescription, holder.employee);
 	}
 
-	@When("the employee edits start date to {string} for task with title {string}, description {string}, start date {string} and end date {string}")
-	public void the_employee_edits_start_date_to_for_task_with_title_description_start_date_and_end_date(
-			String newStartDateString, String title, String description, String startDateString, String endDateString) {
+	@Then("A task with description {string} exists")
+	public void a_task_with_description_exists(String newDescription) {
+		ArrayList<Task> tasks = holder.project.getTasks();
+		assertThat(tasks, contains(hasProperty("description", is(newDescription))));
+	}
+
+	@Then("the task with description {string} does not exist")
+	public void the_task_with_description_does_not_exist(String newDescription) {
+		ArrayList<Task> tasks = holder.project.getTasks();
+		assertThat(tasks, contains(hasProperty("description", not(is(newDescription)))));
+	}
+	
+	@When("the employee edits start date to {string} for task")
+	public void the_employee_edits_start_date_to_for_task(String newStartDateString) {
 		Date newStartDate = Date.FromString(newStartDateString);
-		Date startDate = Date.FromString(startDateString);
-		Date endDate = Date.FromString(endDateString);
-		Task task = holder.project.findTask(title, description, startDate, endDate);
 		holder.project.editTaskStartDate(task, newStartDate, holder.employee);
 	}
 
-	@When("the employee edits end date to {string} for task with title {string}, description {string}, start date {string} and end date {string}")
-	public void the_employee_edits_end_date_to_for_task_with_title_description_start_date_and_end_date(
-			String newEndDateString, String title, String description, String startDateString, String endDateString) {
+	@Then("A task with start date {string} exists")
+	public void a_task_with_start_date_exists(String newStartDateString) {
+		Date newStartDate = Date.FromString(newStartDateString);
+		ArrayList<Task> tasks = holder.project.getTasks();
+		assertThat(tasks, contains(hasProperty("startDate", is(newStartDate))));
+	}
+
+	@Then("the task with start date {string} does not exist")
+	public void the_task_with_start_date_does_not_exist(String newStartDateString) {
+		Date newStartDate = Date.FromString(newStartDateString);
+		ArrayList<Task> tasks = holder.project.getTasks();
+		assertThat(tasks, contains(hasProperty("startDate", not(is(newStartDate)))));
+	}
+
+	@When("the employee edits end date to {string} for task")
+	public void the_employee_edits_end_date_to_for_task(String newEndDateString) {
 		Date newEndDate = Date.FromString(newEndDateString);
-		Date startDate = Date.FromString(startDateString);
-		Date endDate = Date.FromString(endDateString);
-		Task task = holder.project.findTask(title, description, startDate, endDate);
 		holder.project.editTaskEndDate(task, newEndDate, holder.employee);
 	}
 
-	@When("the employee edits expected time to {double} hours for task with title {string}, description {string}, start date {string} and end date {string}")
-	public void the_employee_edits_expected_time_to_hours_for_task_with_title_description_start_date_and_end_date(
-			Double newExpectedTime, String title, String description, String startDateString, String endDateString) {
-		Date startDate = Date.FromString(startDateString);
-		Date endDate = Date.FromString(endDateString);
-		Task task = holder.project.findTask(title, description, startDate, endDate);
-		holder.project.editExpectedTimeForTask(task, newExpectedTime, holder.employee);
+	@Then("A task with end date {string} exists")
+	public void a_task_with_end_date_exists(String newEndDateString) {
+		Date newEndDate = Date.FromString(newEndDateString);
+		ArrayList<Task> tasks = holder.project.getTasks();
+		assertThat(tasks, contains(hasProperty("endDate", is(newEndDate))));
 	}
 
-	@Then("the task with title {string}, description {string}, start date {string} and end date {string} has {double} hours of expected time")
-	public void the_task_with_title_description_start_date_and_end_date_has_hours_of_expected_time(String title,
-			String description, String startDateString, String endDateString, Double newExpectedTime) {
-		Date startDate = Date.FromString(startDateString);
-		Date endDate = Date.FromString(endDateString);
-		Task task = holder.project.findTask(title, description, startDate, endDate);
+	@Then("the task with end date {string} does not exist")
+	public void the_task_with_end_date_does_not_exist(String newEndDateString) {
+		Date newEndDate = Date.FromString(newEndDateString);
+		ArrayList<Task> tasks = holder.project.getTasks();
+		assertThat(tasks, contains(hasProperty("endDate", not(is(newEndDate)))));
+	}
+	
+	@Given("the task has {double} hours of expected time")
+	public void the_task_has_hours_of_expected_time(Double newExpectedtime) {
+		task.setExpectedTime(newExpectedtime);
+	}
+
+	@When("the employee edits expected time to {double} hours for task")
+	public void the_employee_edits_expected_time_to_hours_for_task(Double newExpectedTime) {
+		holder.project.editExpectedTimeForTask(task, newExpectedTime, holder.employee);
+	}
+	
+	@Then("the task now has {double} hours of expected time")
+	public void the_task_now_has_hours_of_expected_time(Double newExpectedTime) {
 		assertThat(task.getExpectedTime(), is(equalTo(newExpectedTime)));
 	}
+
 
 }
