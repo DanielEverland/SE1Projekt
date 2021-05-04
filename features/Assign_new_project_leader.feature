43f9d5577bb42ca66705a4bc065fa178abda12d0
ThorbjornThanner <76940151+ThorbjornThanner@users.noreply.github.com>
2021-05-04 17:40:57 +0200

    Cleanup

--- features/Assign_new_project_leader.feature
+++ features/Assign_new_project_leader.feature
@@ -3,14 +3,14 @@ Description: New project leader is assigned to a project
 Actor: Employee
 
 Scenario: An employee is assigned as project leader to project
-Given the project with title "default_test_project" exists
+Given The project with title "default_test_project" exists
 And an employee with id "test" exists in the application
 And the employee is signed in
 When an employee enters the ID of employee "test" who shall be the project manager of the project with title "default_test_project"
 Then the employee with ID "test" is now the project manager of the project with title "default_test_project"
 
 Scenario: An employee tries to assign a project leader but the project already has a project leader
-Given the project with title "default_test_project" exists
+Given The project with title "default_test_project" exists
 And an employee with id "test_project_Leader" exists in the application
 And an employee with id "test" exists in the application
 And the employee is signed in
--- src/test/java/ProjectManagement/AssignNewProjectLeader.java
+++ src/test/java/ProjectManagement/AssignNewProjectLeader.java
@@ -13,7 +13,7 @@ public class AssignNewProjectLeader {
 		this.holder = holder;
 	}
 
-	@Given("the project with title {string} exists")
+	@Given("The project with title {string} exists")
 	public void the_project_with_title_exists(String title) throws AuthException {
 		holder.app.createProject(title);
 	}
@@ -21,21 +21,21 @@ public class AssignNewProjectLeader {
 	@When("an employee enters the ID of employee {string} who shall be the project manager of the project with title {string}")
 	public void an_employee_enters_the_id_of_employee_who_shall_be_the_project_manager_of_the_project_with_title(
 			String employeeID, String title) {
-		Project project = holder.app.getProjectByTitle(title);
+		Project project = holder.app.getSpecificProjectByTitle(title);
 		project.assignProjectLeader(holder.app.getEmployee(employeeID));
 	}
 
 	@Then("the employee with ID {string} is now the project manager of the project with title {string}")
 	public void the_employee_with_id_is_now_the_project_manager_of_the_project_with_title(String employeeID,
 			String title) {
-		Project project = holder.app.getProjectByTitle(title);
+		Project project = holder.app.getSpecificProjectByTitle(title);
 		assertTrue(project.isProjectLeader(holder.app.getEmployee(employeeID)));
 	}
 
 	@Given("the project with title {string} already has an assigned project leader with ID {string}")
 	public void the_project_with_title_already_has_an_assigned_project_leader_with_id(String title, String employeeID) {
 
-		Project project = holder.app.getProjectByTitle(title);
+		Project project = holder.app.getSpecificProjectByTitle(title);
 		if (!project.hasProjectLeader()) {
 			project.assignProjectLeader(holder.app.getEmployee(employeeID));
 		}
@@ -44,7 +44,7 @@ public class AssignNewProjectLeader {
 
 	@Then("the employee with id {string} remains the project leader of the project {string}")
 	public void the_employee_with_id_remains_the_project_leader_of_the_project(String employeeID, String title) {
-		Project project = holder.app.getProjectByTitle(title);
+		Project project = holder.app.getSpecificProjectByTitle(title);
 		project.assignProjectLeader(holder.app.getEmployee(employeeID));
 		assertTrue(project.getProjectLeader() == holder.app.getEmployee(employeeID));
 	}
