27d0f6970beccabbc9bcd56f1c7840aa5f18a713
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2021-05-04 15:50:48 +0200

    Create SeeAllTasksSteps.java

---
+++ src/test/java/ProjectManagement/SeeAllTasksSteps.java
@@ -0,0 +1,33 @@
+package ProjectManagement;
+
+import java.util.ArrayList;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+public class SeeAllTasksSteps {
+	
+	MainHolder holder;
+	private ArrayList<Task> allTasks;
+	
+	public SeeAllTasksSteps(MainHolder holder) {
+		this.holder = holder;
+	}
+	
+	@When("the employee clicks to see all tasks in the application")
+	public void the_employee_clicks_to_see_all_tasks_in_the_application() {
+		allTasks = holder.app.seeAllTasks();
+	}
+
+	@Then("all tasks in the application are shown")
+	public void all_tasks_in_the_application_are_shown() {
+		ArrayList<Task> tasks = holder.app.seeAllTasks();
+		assertThat(allTasks.equals(tasks), is(equalTo(true)));
+//		idk kan ikke komme på noget som ikke er dumt
+	}
+
+}
